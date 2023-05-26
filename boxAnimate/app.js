8eab17940da6ce441ce4031f389bd2e53895a3fb
evilkeil <114153280+evilkeil@users.noreply.github.com>
2023-05-26 18:52:56 +0900

    fix logic by making it shck if it has grow nad shrink and perform accordingly
    the old ver didnt have the logic to check if it is at the shrunken state so the animation/change didnt always apply when clicked

--- boxAnimate/app.js
+++ boxAnimate/app.js
@@ -2,12 +2,15 @@ const box = document.querySelector('.box');
 const trigger = document.querySelector('.trigger');
 
 trigger.addEventListener('click',function(){
-    if (box.classList.contains("growleft")){
-        box.classList.toggle('growleft');
-        box.classList.toggle('shrink-left');
+    if (box.classList.contains("growleft") && !(box.classList.contains("shrink-left")) ){
+        box.classList.remove('growleft');
+        box.classList.add('shrink-left');
     }
-    else{
-        box.classList.toggle('growleft');
+    else if (box.classList.contains("shrink-left") && !(box.classList.contains("growleft")) ){
+        box.classList.add('growleft');
+        box.classList.remove('shrink-left')
+    }else if (!(box.classList.contains("shrink-left")) && !(box.classList.contains("growleft")) ){
+        box.classList.add('growleft');
     }
 })
 
