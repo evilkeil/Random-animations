c8c693eb3381ae72d096208e7fa1f93f61fb300c
evilkeil <114153280+evilkeil@users.noreply.github.com>
2023-05-26 21:00:34 +0900

    create function to animate and put them in const

--- boxAnimate/app.js
+++ boxAnimate/app.js
@@ -1,16 +1,48 @@
+// const box = document.querySelector('.box');
+// const trigger = document.querySelector('.trigger');
+
+// trigger.addEventListener('click',function(){
+//     if (box.classList.contains("growleft") && !(box.classList.contains("shrink-left")) ){
+//         box.classList.remove('growleft');
+//         box.classList.add('shrink-left');
+//     }
+//     else if (box.classList.contains("shrink-left") && !(box.classList.contains("growleft")) ){
+//         box.classList.add('growleft');
+//         box.classList.remove('shrink-left')
+//     }else if (!(box.classList.contains("shrink-left")) && !(box.classList.contains("growleft")) ){
+//         box.classList.add('growleft');
+//     }
+// })
+
 const box = document.querySelector('.box');
 const trigger = document.querySelector('.trigger');
 
+//arguments 
+
+const growRight = () => animate("grow-right", "shrink-right");
+const growLeft = () => animate("grow-left", "shrink-left");
+const growUp = () => animate("grow-up", "shrink-up");
+const growDown = () => animate("grow-down", "shrink-down");
+
+//functions
+
+function animate(grow,shrink){
+    if (box.classList.contains(grow) && !(box.classList.contains(shrink)) ){
+        box.classList.remove(grow);
+        box.classList.add(shrink);
+    }
+    else if (box.classList.contains(shrink) && !(box.classList.contains(grow)) ){
+        box.classList.add(grow);
+        box.classList.remove(shrink)
+    }else if (!(box.classList.contains(shrink)) && !(box.classList.contains(grow)) ){
+        box.classList.add(grow);
+    }
+}
+
+
 trigger.addEventListener('click',function(){
-    if (box.classList.contains("growleft") && !(box.classList.contains("shrink-left")) ){
-        box.classList.remove('growleft');
-        box.classList.add('shrink-left');
-    }
-    else if (box.classList.contains("shrink-left") && !(box.classList.contains("growleft")) ){
-        box.classList.add('growleft');
-        box.classList.remove('shrink-left')
-    }else if (!(box.classList.contains("shrink-left")) && !(box.classList.contains("growleft")) ){
-        box.classList.add('growleft');
-    }
+    let rand = Math.floor(Math.random() * 4);
+    growRight();
 })
+
 
