f1f43dbced80d807508cfe15732c02e7c2bf3d7a
evilkeil <114153280+evilkeil@users.noreply.github.com>
2023-05-27 04:27:36 +0900

    create a function to animate it based on the generated direction

--- boxAnimate/test.js
+++ boxAnimate/test.js
@@ -10,46 +10,79 @@ function getRandomDirection() {
 }
 //check whether a direction is generated or not
 function checkDirection(){
-    if (animationDirection ===""){
+    if (!(animationDirection ==="")){
         return true;
     }else return false;
 }
 
 // Toggle animation classes based on the current state
+
 function toggleAnimation() {
-    animationDirection = getRandomDirection();
-    console.log(animationDirection);
-    if (!box.classList.contains('original')) {
-        box.classList.remove('shrink-up', 'shrink-down', 'shrink-left', 'shrink-right');
-        box.classList.remove('grow-up', 'grow-down', 'grow-left', 'grow-right');
+    const directionAvailable = checkDirection();
+    if ( directionAvailable === false){
+        animationDirection = getRandomDirection();
+            box.classList.remove('shrink-right', 'shrink-up', 'shrink-down', 'shrink-left');
+            box.classList.add(animationDirection);
+            box.classList.remove('original');
+    }else{
+        if (!(box.classList.contains('original'))){
+            box.classList.remove('shrink-up', 'shrink-down', 'shrink-left', 'shrink-right');
             if(animationDirection==='grow-up'){
-            box.classList.add('shrink-up');
-            box.classList.add('original');
-            animationDirection = null;
-            } 
+                box.classList.remove('grow-up');
+                box.classList.add('shrink-up');
+                box.classList.add('original');
+                } 
             else if(animationDirection=== 'grow-down'){
+                box.classList.remove('grow-down');
                 box.classList.add('shrink-down');
                 box.classList.add('original');
-                animationDirection = null;
                 } 
             else if(animationDirection==='grow-left'){
+                box.classList.remove('grow-left');
                 box.classList.add('shrink-left');
                 box.classList.add('original');
-                animationDirection = null;
                 } 
             else if(animationDirection==='grow-right'){
+                box.classList.remove('grow-right');
                 box.classList.add('shrink-right');
                 box.classList.add('original');
-                animationDirection = null;
                 }   
-                
-    } else {
-        
-        box.classList.remove('shrink-right', 'shrink-up', 'shrink-down', 'shrink-left');
-        box.classList.add(animationDirection);
-        box.classList.remove('original');
+            animationDirection ="";
+        }
     }
 }
+
+
+// function toggleAnimation() {
+//     animationDirection = getRandomDirection();
+//     console.log(animationDirection);
+//     if (!box.classList.contains('original')) {
+//         box.classList.remove('shrink-up', 'shrink-down', 'shrink-left', 'shrink-right');
+//         box.classList.remove('grow-up', 'grow-down', 'grow-left', 'grow-right');
+//             if(animationDirection==='grow-up'){
+//             box.classList.add('shrink-up');
+//             box.classList.add('original');
+//             } 
+//             else if(animationDirection=== 'grow-down'){
+//                 box.classList.add('shrink-down');
+//                 box.classList.add('original');
+//                 } 
+//             else if(animationDirection==='grow-left'){
+//                 box.classList.add('shrink-left');
+//                 box.classList.add('original');
+//                 } 
+//             else if(animationDirection==='grow-right'){
+//                 box.classList.add('shrink-right');
+//                 box.classList.add('original');
+//                 }   
+                
+//     } else {
+        
+//         box.classList.remove('shrink-right', 'shrink-up', 'shrink-down', 'shrink-left');
+//         box.classList.add(animationDirection);
+//         box.classList.remove('original');
+//     }
+// }
 
 // Event listener
 trigger.addEventListener('click', toggleAnimation);
